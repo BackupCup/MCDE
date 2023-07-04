0142eb7db5ecbcfd34538af1870493717c701486
endlo <endlone000@gmail.com>
2023-07-04 17:45:09 +0300

    Added logic for displaying the amount of slots and the opening/closing of their UI.

--- src/main/java/net/backupcup/mcd_enchantments/screen/RunicTableScreen.java
+++ src/main/java/net/backupcup/mcd_enchantments/screen/RunicTableScreen.java
@@ -1,8 +1,9 @@
 package net.backupcup.mcd_enchantments.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-
 import net.backupcup.mcd_enchantments.MCDEnchantments;
+import net.backupcup.mcd_enchantments.util.EnchantmentSlots;
+import net.backupcup.mcd_enchantments.util.Slot;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -15,12 +16,10 @@ import net.minecraft.util.Identifier;
 public class RunicTableScreen extends HandledScreen<RunicTableScreenHandler> {
     private Inventory inventory;
 
+    private boolean[] slotOpened = new boolean[3];
+
     private static final Identifier TEXTURE =
             new Identifier(MCDEnchantments.MOD_ID, "textures/gui/runic_table.png");
-    private static final Identifier ENCHANTMENT_ICONS_DUNGEONS =
-            new Identifier(MCDEnchantments.MOD_ID, "textures/gui/enchantment_icons_dungeons.png");
-    private static final Identifier ENCHANTMENT_ICONS_VANILLA =
-            new Identifier(MCDEnchantments.MOD_ID, "textures/gui/enchantment_icons_vanilla.png");
 
     public RunicTableScreen(RunicTableScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -49,15 +48,57 @@ public class RunicTableScreen extends HandledScreen<RunicTableScreenHandler> {
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
         ItemStack stack = inventory.getStack(0);
 
+        int posX = ((width - backgroundWidth) / 2) - 2;
+        int posY = (height - backgroundHeight) / 2;
+        int[] slotOffsetsX = {posX + 17, posX + 52, posX + 87};
+
+        if (stack.isEmpty()) return super.mouseClicked(mouseX, mouseY, button);
+        EnchantmentSlots slots = EnchantmentSlots.fromNbt(stack.getNbt().getCompound("Slots"));
+
+        for (Slot slot : Slot.values()) {
+            if (slots.getSlot(slot).isEmpty()) continue;
+
+            if (isInSBounds(slotOffsetsX[slot.ordinal()], posY + 37, (int) mouseX, (int) mouseY)) {
+                for (int j = 0; j < 3; j++) {
+                    slotOpened[j] = slot.ordinal() == j && !slotOpened[j];
+                }
+                break;
+            }
+        }
+
         return super.mouseClicked(mouseX, mouseY, button);
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        ItemStack stack = inventory.getStack(0);
-
         renderBackground(matrices);
         super.render(matrices, mouseX, mouseY, delta);
+
+        int posX = ((width - backgroundWidth) / 2) - 2;
+        int posY = (height - backgroundHeight) / 2;
+        int[] slotOffsetsX = {posX + 17, posX + 52, posX + 87};
+        
+        ItemStack itemStack = inventory.getStack(0);
+
+        if (itemStack.isEmpty()) {
+            drawMouseoverTooltip(matrices, mouseX, mouseY);
+            return;
+        }
+
+        EnchantmentSlots slots = EnchantmentSlots.fromNbt(itemStack.getNbt().getCompound("Slots"));
+
+        for (Slot slot : Slot.values()) {
+            if (slots.getSlot(slot).isEmpty()) continue;
+
+            drawTexture(matrices, slotOffsetsX[slot.ordinal()] + 1, posY + 38, 187, 105, 31, 31);
+            if (isInSBounds(slotOffsetsX[slot.ordinal()], posY + 37, mouseX, mouseY))
+                drawTexture(matrices, slotOffsetsX[slot.ordinal()], posY + 37, 220, 104, 33, 33);
+
+            if (slotOpened[slot.ordinal()]) {
+                drawTexture(matrices, posX + (slot.ordinal() * 35), posY, 186, 0, 67, 51);
+            }
+        }
+
         drawMouseoverTooltip(matrices, mouseX, mouseY);
     }
 
