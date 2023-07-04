4abe062a93f07ab3665cf621bd55498235d95517
endlo <endlone000@gmail.com>
2023-07-04 17:44:05 +0300

    Moved the block to the Vanilla Decorations group

--- src/main/java/net/backupcup/mcd_enchantments/block/ModBlocks.java
+++ src/main/java/net/backupcup/mcd_enchantments/block/ModBlocks.java
@@ -2,7 +2,6 @@ package net.backupcup.mcd_enchantments.block;
 
 import net.backupcup.mcd_enchantments.MCDEnchantments;
 import net.backupcup.mcd_enchantments.block.custom.RunicTableBlock;
-import net.backupcup.mcd_enchantments.item.ModItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -16,7 +15,7 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
 
     public static final Block RUNIC_TABLE = registerBlock("runic_table",
-            new RunicTableBlock(FabricBlockSettings.of(Material.WOOD).strength(2f).nonOpaque()), ModItemGroup.MCDEncahntments);
+            new RunicTableBlock(FabricBlockSettings.of(Material.WOOD).strength(2f).nonOpaque()), ItemGroup.DECORATIONS);
 
     public static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
