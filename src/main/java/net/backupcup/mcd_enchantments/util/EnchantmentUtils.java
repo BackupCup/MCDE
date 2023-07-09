07f174462aea6c546105ab88d3394a6b76eb972f
endlo <endlone000@gmail.com>
2023-07-09 16:13:28 +0300

    sus

--- src/main/java/net/backupcup/mcd_enchantments/util/EnchantmentUtils.java
+++ src/main/java/net/backupcup/mcd_enchantments/util/EnchantmentUtils.java
@@ -1,31 +1,5 @@
 package net.backupcup.mcd_enchantments.util;
 
-import static net.backupcup.mcd_enchantments.util.Slots.FIRST;
-import static net.backupcup.mcd_enchantments.util.Slots.SECOND;
-import static net.backupcup.mcd_enchantments.util.Slots.THIRD;
-import static net.minecraft.enchantment.Enchantments.CHANNELING;
-import static net.minecraft.enchantment.Enchantments.EFFICIENCY;
-import static net.minecraft.enchantment.Enchantments.FORTUNE;
-import static net.minecraft.enchantment.Enchantments.IMPALING;
-import static net.minecraft.enchantment.Enchantments.LOYALTY;
-import static net.minecraft.enchantment.Enchantments.LUCK_OF_THE_SEA;
-import static net.minecraft.enchantment.Enchantments.LURE;
-import static net.minecraft.enchantment.Enchantments.MENDING;
-import static net.minecraft.enchantment.Enchantments.RIPTIDE;
-import static net.minecraft.enchantment.Enchantments.SILK_TOUCH;
-import static net.minecraft.enchantment.Enchantments.UNBREAKING;
-import static net.minecraft.util.registry.Registry.ENCHANTMENT;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
-import java.util.function.Predicate;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import net.backupcup.mcd_enchantments.MCDEnchantments;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
@@ -33,6 +7,16 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static net.backupcup.mcd_enchantments.util.Slots.*;
+import static net.minecraft.enchantment.Enchantments.*;
+import static net.minecraft.util.registry.Registry.ENCHANTMENT;
 
 public class EnchantmentUtils {
     private static Predicate<String> namespaceMatcher = Pattern.compile("minecraft|mcd[aw]").asPredicate();
@@ -71,37 +55,23 @@ public class EnchantmentUtils {
 
     public static EnchantmentSlots getEnchantments(ItemStack itemStack) {
         var item = itemStack.getItem();
-        if (EnchantmentTarget.DIGGER.isAcceptableItem(item)) {
+        // itemStack.isIn(ModTags.Items.WEAPONS) ||
+        if (EnchantmentTarget.TRIDENT.isAcceptableItem(item)) {
             return EnchantmentSlots.builder()
-                .withSlot(FIRST, getEnchantmentId(EFFICIENCY))
-                .withSlot(SECOND,
-                    getEnchantmentId(FORTUNE),
-                    getEnchantmentId(SILK_TOUCH)
-                )
-                .build();
-        }
-        else if (EnchantmentTarget.FISHING_ROD.isAcceptableItem(item)) {
-            return EnchantmentSlots.builder()
-                .withSlot(FIRST, getEnchantmentId(LURE))
-                .withSlot(SECOND, getEnchantmentId(LUCK_OF_THE_SEA))
-                .build();
-        }
-        else if (EnchantmentTarget.TRIDENT.isAcceptableItem(item)) {
-            return EnchantmentSlots.builder()
-                .withSlot(FIRST,
-                    getEnchantmentId(IMPALING),
-                    getEnchantmentId(LOYALTY)
-                )
-                .withSlot(SECOND,
-                    getEnchantmentId(RIPTIDE),
-                    getEnchantmentId(CHANNELING)
-                )
-                .build();
+                    .withSlot(FIRST,
+                            getEnchantmentId(IMPALING),
+                            getEnchantmentId(LOYALTY)
+                    )
+                    .withSlot(SECOND,
+                            getEnchantmentId(RIPTIDE),
+                            getEnchantmentId(CHANNELING)
+                    )
+                    .build();
         }
         else if (EnchantmentTarget.WEAPON.isAcceptableItem(item) ||
-                 EnchantmentTarget.BOW.isAcceptableItem(item) || EnchantmentTarget.CROSSBOW.isAcceptableItem(item) ||
-                 EnchantmentTarget.ARMOR_FEET.isAcceptableItem(item) || EnchantmentTarget.ARMOR_LEGS.isAcceptableItem(item) ||
-                 EnchantmentTarget.ARMOR_CHEST.isAcceptableItem(item) || EnchantmentTarget.ARMOR_HEAD.isAcceptableItem(item)) {
+                EnchantmentTarget.BOW.isAcceptableItem(item) || EnchantmentTarget.CROSSBOW.isAcceptableItem(item) ||
+                EnchantmentTarget.ARMOR_FEET.isAcceptableItem(item) || EnchantmentTarget.ARMOR_LEGS.isAcceptableItem(item) ||
+                EnchantmentTarget.ARMOR_CHEST.isAcceptableItem(item) || EnchantmentTarget.ARMOR_HEAD.isAcceptableItem(item)) {
             Random random = new Random(System.nanoTime());
             List<Identifier> enchantments = getEnchantmentsForItem(itemStack);
             Collections.shuffle(enchantments, random);
@@ -144,6 +114,22 @@ public class EnchantmentUtils {
 
             return builder.build();
         }
+        else if (EnchantmentTarget.DIGGER.isAcceptableItem(item)) {
+            return EnchantmentSlots.builder()
+                .withSlot(FIRST, getEnchantmentId(EFFICIENCY))
+                .withSlot(SECOND,
+                    getEnchantmentId(FORTUNE),
+                    getEnchantmentId(SILK_TOUCH)
+                )
+                .build();
+        }
+        else if (EnchantmentTarget.FISHING_ROD.isAcceptableItem(item)) {
+            return EnchantmentSlots.builder()
+                .withSlot(FIRST, getEnchantmentId(LURE))
+                .withSlot(SECOND, getEnchantmentId(LUCK_OF_THE_SEA))
+                .build();
+        }
+        else
         MCDEnchantments.LOGGER.warn("Empty slots generated");
         return EnchantmentSlots.EMPTY;
     }
