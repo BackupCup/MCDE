9b6cdacbe07531cfa585f5d500d7ac0de8c1c203
endlo <endlone000@gmail.com>
2023-07-10 17:48:39 +0300

    Added a check to block enchanted tool/weapon merging

--- src/main/java/net/backupcup/mcd_enchantments/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/net/backupcup/mcd_enchantments/mixin/AnvilScreenHandlerMixin.java
@@ -1,14 +1,15 @@
 package net.backupcup.mcd_enchantments.mixin;
 
+import net.backupcup.mcd_enchantments.MCDEnchantments;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.AnvilScreenHandler;
+import net.minecraft.screen.slot.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import net.backupcup.mcd_enchantments.MCDEnchantments;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.screen.AnvilScreenHandler;
-import net.minecraft.screen.slot.Slot;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class AnvilScreenHandlerMixin {
@@ -18,7 +19,10 @@ public abstract class AnvilScreenHandlerMixin {
 
     @Inject(method = "canTakeOutput", at = @At("HEAD"), cancellable = true)
     protected void canTakeOutputProxy(PlayerEntity player, boolean present, CallbackInfoReturnable<Boolean> cir) {
-        MCDEnchantments.LOGGER.info("canTakeOutput called with slots {} and {}", screenGetSlot(0).getStack(), screenGetSlot(1).getStack());
-        cir.setReturnValue(false);
+        ItemStack itemSlot1 = screenGetSlot(0).getStack();
+        ItemStack itemSlot2 = screenGetSlot(1).getStack();
+
+        MCDEnchantments.LOGGER.info("canTakeOutput called with slots {} and {}", itemSlot1, itemSlot2);
+        if ((itemSlot2.getItem() != Items.ENCHANTED_BOOK)) cir.setReturnValue(false);
     }
 }
