05810742550c204a765c1cca767c692dde6227bd
endlo <endlone000@gmail.com>
2023-07-12 13:41:18 +0300

    Added UI code for Reroll Station

--- src/main/java/net/backupcup/mcd_enchantments/MCDEClient.java
+++ src/main/java/net/backupcup/mcd_enchantments/MCDEClient.java
@@ -2,6 +2,7 @@ package net.backupcup.mcd_enchantments;
 
 import net.backupcup.mcd_enchantments.block.ModBlocks;
 import net.backupcup.mcd_enchantments.screen.ModScreenHandlers;
+import net.backupcup.mcd_enchantments.screen.RerollStationScreen;
 import net.backupcup.mcd_enchantments.screen.RunicTableScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
@@ -12,6 +13,7 @@ public class MCDEClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HandledScreens.register(ModScreenHandlers.RUNIC_TABLE_SCREEN_HANDLER, RunicTableScreen::new);
+        HandledScreens.register(ModScreenHandlers.REROLL_STATION_SCREEN_HANDLER, RerollStationScreen::new);
 
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RUNIC_TABLE, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.REROLL_STATION, RenderLayer.getCutout());
---
+++ src/main/java/net/backupcup/mcd_enchantments/screen/RerollStationScreenHandler.java
@@ -0,0 +1,96 @@
+package net.backupcup.mcd_enchantments.screen;
+
+import net.backupcup.mcd_enchantments.util.EnchantmentSlots;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ArrayPropertyDelegate;
+import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
+
+public class RerollStationScreenHandler extends ScreenHandler {
+    private final Inventory inventory;
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    private final PropertyDelegate propertyDelegate;
+
+    public RerollStationScreenHandler(int syncId, PlayerInventory inventory) {
+        this(syncId, inventory, new SimpleInventory(1), new ArrayPropertyDelegate(0));
+    }
+
+    public RerollStationScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, PropertyDelegate delegate) {
+        super(ModScreenHandlers.REROLL_STATION_SCREEN_HANDLER, syncId);
+
+        checkSize(inventory, 1);
+        this.inventory = inventory;
+        inventory.onOpen(playerInventory.player);
+        this.propertyDelegate = delegate;
+
+        this.addSlot(new Slot(inventory, 0, 145, 46) {
+
+            @Override
+            public boolean canInsert(ItemStack stack) {
+                return (EnchantmentSlots.fromItemStack(stack) != null);
+            }
+
+            @Override
+            public int getMaxItemCount() {
+                return 1;
+            }
+        });
+
+        addPlayerInventory(playerInventory);
+        addPlayerHotbar(playerInventory);
+
+        addProperties(delegate);
+    }
+
+    @Override
+    public ItemStack transferSlot(PlayerEntity player, int invSlot) {
+        ItemStack newStack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(invSlot);
+        if (slot != null && slot.hasStack()) {
+            ItemStack originalStack = slot.getStack();
+            newStack = originalStack.copy();
+            if (invSlot < this.inventory.size()) {
+                if (!this.insertItem(originalStack, this.inventory.size(), this.slots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.insertItem(originalStack, 0, this.inventory.size(), false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (originalStack.isEmpty()) {
+                slot.setStack(ItemStack.EMPTY);
+            } else {
+                slot.markDirty();
+            }
+        }
+
+        return newStack;
+    }
+
+    @Override
+    public boolean canUse(PlayerEntity player) {
+        return this.inventory.canPlayerUse(player);
+    }
+
+    private void addPlayerInventory(PlayerInventory playerInventory) {
+        for (int i = 0; i < 3; ++i) {
+            for (int l = 0; l < 9; ++l) {
+                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 10 + l * 18, 84 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(PlayerInventory playerInventory) {
+        for (int i = 0; i < 9; ++i) {
+            this.addSlot(new Slot(playerInventory, i, 10 + i * 18, 142));
+        }
+    }
+}
