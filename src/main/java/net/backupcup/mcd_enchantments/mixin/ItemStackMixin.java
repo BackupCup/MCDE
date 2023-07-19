827aff3f057f1c87909c9803725379b66c337a68
Iamnotagenius <kazakk9@gmail.com>
2023-07-19 22:43:13 +0300

    fix: tooltip doesn't show enchantments provided by other mods

--- src/main/java/net/backupcup/mcd_enchantments/mixin/ItemStackMixin.java
+++ src/main/java/net/backupcup/mcd_enchantments/mixin/ItemStackMixin.java
@@ -1,12 +1,15 @@
 package net.backupcup.mcd_enchantments.mixin;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -33,16 +36,32 @@ public abstract class ItemStackMixin {
 
     @Shadow public static void appendEnchantments(List<Text> tooltip, NbtList list) { }
 
-    @Redirect(
+    @ModifyArg(
         method = "getTooltip",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/item/ItemStack;appendEnchantments(Ljava/util/List;Lnet/minecraft/nbt/NbtList;)V",
             ordinal = 0
-        )
+        ),
+        index = 1
     )
-    private void nullifyAppendEnchantments(List<Text> tooltip, NbtList list) {
-        return;
+    private NbtList removeMcdeManagedEnchantments(NbtList list) {
+        var itemStack = (ItemStack)(Object)this;
+        var slots = EnchantmentSlots.fromItemStack(itemStack);
+
+        if (slots == null) {
+            return list;
+        }
+        var map = EnchantmentHelper.get(itemStack);
+        for (var slot : slots) {
+            slot.getChosen().ifPresent(chosen -> map.remove(chosen.getEnchantment()));
+        }
+        if (nbt.contains("Gilding")) {
+            map.remove(Registry.ENCHANTMENT.get(Identifier.tryParse(nbt.getString("Gilding"))));
+        }
+        return map.entrySet().stream()
+            .map(kvp -> EnchantmentHelper.createNbt(EnchantmentHelper.getEnchantmentId(kvp.getKey()), kvp.getValue()))
+            .collect(Collectors.toCollection(() -> new NbtList()));
     }
 
     @Inject(
@@ -50,15 +69,12 @@ public abstract class ItemStackMixin {
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/item/ItemStack;appendEnchantments(Ljava/util/List;Lnet/minecraft/nbt/NbtList;)V",
-            ordinal = 0
+            ordinal = 0,
+            shift = Shift.AFTER
         ),
         locals = LocalCapture.CAPTURE_FAILHARD
     )
     private void appendEnchantmentLines(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir, List<Text> tooltip) {
-        if (nbt.contains("Enchantments")) {
-            appendEnchantments(tooltip, nbt.getList("Enchantments", NbtElement.COMPOUND_TYPE));
-        }
-
         var slots = EnchantmentSlots.fromItemStack((ItemStack)(Object)this);
         if (slots == null) {
             return;
@@ -67,8 +83,8 @@ public abstract class ItemStackMixin {
         for (var slot : slots) {
             if (slot.getChosen().isPresent()) {
                 var chosen =  slot.getChosen().get();
-                var name = Text.translatable(chosen.getEnchantment().toTranslationKey("enchantment"));
-                if (Registry.ENCHANTMENT.get(chosen.getEnchantment()).getMaxLevel() > 1) {
+                var name = Text.translatable(chosen.getEnchantmentId().toTranslationKey("enchantment"));
+                if (Registry.ENCHANTMENT.get(chosen.getEnchantmentId()).getMaxLevel() > 1) {
                     name.append(" ")
                         .append(Text.translatable("enchantment.level." + chosen.getLevel()));
                 }
@@ -97,7 +113,7 @@ public abstract class ItemStackMixin {
 
         for (var slot : slots) {
             slot.getChosen().ifPresent(chosen -> {
-                newList.add(EnchantmentHelper.createNbt(chosen.getEnchantment(), chosen.getLevel()));
+                newList.add(EnchantmentHelper.createNbt(chosen.getEnchantmentId(), chosen.getLevel()));
             });
         }
 
