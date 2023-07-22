9638eef16b853d758fefcd5e37bdcf65b2f03d02
Iamnotagenius <kazakk9@gmail.com>
2023-07-22 16:47:36 +0300

    fix: instead of crash, proceed with the defaults and warn players

--- src/main/java/net/backupcup/mcd_enchantments/MCDEnchantments.java
+++ src/main/java/net/backupcup/mcd_enchantments/MCDEnchantments.java
@@ -3,8 +3,7 @@ package net.backupcup.mcd_enchantments;
 import java.nio.file.Path;
 import java.util.List;
 
-import javax.management.RuntimeErrorException;
-
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.configurate.ConfigurateException;
@@ -15,15 +14,16 @@ import org.spongepowered.configurate.objectmapping.ObjectMapper;
 import org.spongepowered.configurate.objectmapping.meta.Comment;
 import org.spongepowered.configurate.util.NamingSchemes;
 
-import com.typesafe.config.ConfigException;
-
 import net.backupcup.mcd_enchantments.block.ModBlocks;
 import net.backupcup.mcd_enchantments.block.entity.ModBlockEntities;
 import net.backupcup.mcd_enchantments.screen.ModScreenHandlers;
 import net.backupcup.mcd_enchantments.util.IdentifierGlobbedList;
 import net.backupcup.mcd_enchantments.util.IdentifierGlobbedListSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 public class MCDEnchantments implements ModInitializer {
 	public static final String MOD_ID = "mcd_enchantments";
@@ -41,6 +41,13 @@ public class MCDEnchantments implements ModInitializer {
 		ModScreenHandlers.registerAllScreenHandlers();
 		ModBlockEntities.registerBlockEntities();
         config = Config.load();
+
+        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+            if (entity.isPlayer()) {
+                entity.sendMessage(Text.literal("[MCDEnchantments]: ")
+                        .append(Config.lastError()).formatted(Formatting.RED));
+            }
+        });
 	}
 
 
@@ -68,40 +75,50 @@ public class MCDEnchantments implements ModInitializer {
                     );
         }
 
+        private static Text lastError;
+
+        public static @Nullable Text lastError() {
+            return lastError;
+        }
+
         @Comment("Determines whether constraints should allow or deny listed enchantments")
         private ConstraintType constraintType = ConstraintType.DENY;
 
-        @Comment("Lists all enchantments that should (not) be included when using blocks from this mod")
+        @Comment("Lists all enchantments that should (not)\nbe included when using blocks from this mod")
         private IdentifierGlobbedList constraintList = new IdentifierGlobbedList(List.of(
             "minecraft:mending",
             "minecraft:unbreaking"
         ));
 
         public static Config load() {
+            var defaults = new Config();
             try {
                 if (getConfigFile().toFile().exists()) {
+                    lastError = null;
                     return loader.load().get(Config.class);
                 }
                 var root = loader.load();
-                var defaults = new Config();
                 root.set(Config.class, defaults);
                 loader.save(root);
+                lastError = null;
                 return defaults;
             } catch (ConfigurateException e) {
                 if (e instanceof ParsingException pe) {
-                    var parse = (ConfigException.Parse)pe.getCause();
+                    var parse = pe.getCause();
                     // Parse exception message is prefixed by origin description + ": "
                     // So I get rid of it
-                    var origin = parse.origin().description();
+                    var origin = pe.context();
                     LOGGER.error(
-                            "Config syntax error at line {}: {}",
+                            "Config syntax error at line {}: {}. Using defaults.",
                             pe.line(),
                             parse.getMessage().substring(origin.length() + 2)
                             );
-                    throw new RuntimeException(pe);
+                    lastError = Text.translatable("message.mcde.error.config.parsing", pe.line());
+                    return defaults;
                 }
-                LOGGER.error("Error while loading config: {}", e.getMessage());
-                throw new RuntimeException(e);
+                LOGGER.error("Error while loading config: {}. Using defaults.", e.getMessage());
+                lastError = Text.translatable("message.mcde.error.config.general");
+                return defaults;
             }
         }
     }
