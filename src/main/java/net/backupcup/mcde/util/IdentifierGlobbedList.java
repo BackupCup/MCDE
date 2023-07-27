c5d915a823ec3698ac56153f09adb0503c06e263
Iamnotagenius <kazakk9@gmail.com>
2023-07-28 01:23:17 +0300

    feat: add tags support in config

--- src/main/java/net/backupcup/mcde/util/IdentifierGlobbedList.java
+++ src/main/java/net/backupcup/mcde/util/IdentifierGlobbedList.java
@@ -6,19 +6,31 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class IdentifierGlobbedList {
     private final Set<String> namespaces = new HashSet<>();
     private final Set<Identifier> fullySpecified = new HashSet<>();
+    private final Set<Identifier> tags = new HashSet<>();
+    private final Set<String> namespace_tags = new HashSet<>();
 
     public IdentifierGlobbedList(Collection<String> globs) {
         for (var glob : globs) {
             if (glob.endsWith(":*")) {
-                namespaces.add(glob.substring(0, glob.length() - 2));
+                if (glob.startsWith("#")) {
+                    namespace_tags.add(glob.substring(1, glob.length() - 2));
+                } else {
+                    namespaces.add(glob.substring(0, glob.length() - 2));
+                }
             }
             else {
-                fullySpecified.add(Identifier.tryParse(glob));
+                if (glob.startsWith("#")) {
+                    tags.add(Identifier.tryParse(glob.substring(1)));
+                } else {
+                    fullySpecified.add(Identifier.tryParse(glob));
+                }
             }
         }
     }
@@ -31,14 +43,25 @@ public class IdentifierGlobbedList {
                 namespaces.add(namespace);
                 continue;
             }
+            if (paths.size() >= 1 && paths.get(0).equals("#*")) {
+                namespace_tags.add(namespace);
+            }
             for (var path : paths) {
-                fullySpecified.add(Identifier.of(namespace, path));
+                if (path.startsWith("#")) {
+                    tags.add(Identifier.of(namespace, path.substring(1)));
+                } else {
+                    fullySpecified.add(Identifier.of(namespace, path));
+                }
             }
         }
     }
 
     public boolean contains(Identifier id) {
-        return containsNamespaceGlob(id) || fullySpecified.contains(id);
+        return containsNamespaceGlob(id) ||
+            fullySpecified.contains(id) ||
+            tags.stream().anyMatch(tag -> ModTags.isIn(id, TagKey.of(Registry.ENCHANTMENT_KEY, tag))) ||
+            namespace_tags.stream().flatMap(ns -> Registry.ENCHANTMENT.streamTags().filter(tag -> tag.id().getNamespace().equals(ns)))
+                .anyMatch(tag -> ModTags.isIn(Registry.ENCHANTMENT.get(id), tag));
     }
 
     public boolean containsNamespaceGlob(Identifier id) {
@@ -55,7 +78,11 @@ public class IdentifierGlobbedList {
 
     @Override
     public String toString() {
-        return "IdentifierGlobbedList{" +namespaces.toString() + ", " + fullySpecified.toString() + "}";
-    }
+        return String.format("IdentifierGlobbedList{%s, %s, %s, %s}",
+            namespaces.toString(),
+            fullySpecified.toString(),
+            tags.toString(),
+            namespace_tags.toString());
+        }
 
 }
