0bb01d31d90cda582c0d585b8598c8b9bdd5fe7f
Iamnotagenius <kazakk9@gmail.com>
2023-07-27 16:27:23 +0300

    fix: register Screen Handlers properly, fixes #5

--- src/main/java/net/backupcup/mcde/screen/handler/ModScreenHandlers.java
+++ src/main/java/net/backupcup/mcde/screen/handler/ModScreenHandlers.java
@@ -1,15 +1,22 @@
 package net.backupcup.mcde.screen.handler;
 
+import net.backupcup.mcde.MCDEnchantments;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ModScreenHandlers {
-    public static ScreenHandlerType<RunicTableScreenHandler> RUNIC_TABLE_SCREEN_HANDLER;
-    public static ScreenHandlerType<RollBenchScreenHandler> ROLL_BENCH_SCREEN_HANDLER;
-    public static ScreenHandlerType<GildingFoundryScreenHandler> GILDING_FOUNDRY_SCREEN_HANDLER;
+    public static ScreenHandlerType<RunicTableScreenHandler> RUNIC_TABLE_SCREEN_HANDLER = new ScreenHandlerType<>(RunicTableScreenHandler::new);
+    public static ScreenHandlerType<RollBenchScreenHandler> ROLL_BENCH_SCREEN_HANDLER = new ScreenHandlerType<>(RollBenchScreenHandler::new);
+    public static ScreenHandlerType<GildingFoundryScreenHandler> GILDING_FOUNDRY_SCREEN_HANDLER = new ScreenHandlerType<>(GildingFoundryScreenHandler::new);
 
     public static void registerAllScreenHandlers() {
-        RUNIC_TABLE_SCREEN_HANDLER = new ScreenHandlerType<>(RunicTableScreenHandler::new);
-        ROLL_BENCH_SCREEN_HANDLER = new ScreenHandlerType<>(RollBenchScreenHandler::new);
-        GILDING_FOUNDRY_SCREEN_HANDLER = new ScreenHandlerType<>(GildingFoundryScreenHandler::new);
+        registerScreenHandler("runic_table", RUNIC_TABLE_SCREEN_HANDLER);
+        registerScreenHandler("roll_bench", ROLL_BENCH_SCREEN_HANDLER);
+        registerScreenHandler("gilding_foundry", GILDING_FOUNDRY_SCREEN_HANDLER);
+    }
+
+    private static void registerScreenHandler(String id, ScreenHandlerType<?> type) {
+        Registry.register(Registry.SCREEN_HANDLER, Identifier.of(MCDEnchantments.MOD_ID, id), type);
     }
 }
